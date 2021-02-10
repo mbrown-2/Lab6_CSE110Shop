263d64204cc632a85fd5763bc37be8b96ea69fb4
mbrown-2 <66887136+mbrown-2@users.noreply.github.com>
2021-02-10 02:15:39 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,23 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  
+  // Create fetch request to: https://fakestoreapi.com/products
+  fetch('https://fakestoreapi.com/products')
+  .then(function() {
+  
+  })
+  .catch(function() {
+    
+  });
+  
+  myStorage = window.localStorage;
+  
+  
 });
+
+// id, title, price, description, category, image
+
+// Points: fetch data from url
+
+// add array of javascript objects to local storage
